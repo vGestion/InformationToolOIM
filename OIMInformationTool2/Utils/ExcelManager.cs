077eb6ff33c749d183be5426e7c1e934c5babdd3
sofibel98 <88499573+sofibel98@users.noreply.github.com>
2023-01-18 09:22:13 -0500

    Hola

--- OIMInformationTool2/Utils/ExcelManager.cs
+++ OIMInformationTool2/Utils/ExcelManager.cs
@@ -1,6 +1,6 @@
 ﻿using System.Data;
-using System.Data.Odbc;
-using System.Data.OleDb;
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace OIMInformationTool2.Utils
 
@@ -11,30 +11,46 @@ namespace OIMInformationTool2.Utils
 
         public DataSet readExcelSheet(String path_file_xls)
         {
-            // Setting parameters for the excel OleDbConnection
 
-            var connection_String_xls = "Driver={Microsoft Excel Driver (*.xls, *.xlsx, *.xlsm, *.xlsb)};Dbq="+path_file_xls+"; Extensions=xls/xlsx;Persist Security Info=False";
-            // Creating the OleDbConnection connection
-            var connection = new OdbcConnection();
-            connection.ConnectionString = connection_String_xls;
-            // Opening the connection
-            connection.Open();
-            OdbcCommand command = new OdbcCommand();
-            command.Connection = connection;
-            command.CommandType = System.Data.CommandType.Text;
-            // Generating the query
-            command.CommandText = "Select * from [TULCAN_NOMINAL$]";
-            OdbcDataAdapter adapter = new OdbcDataAdapter();
-            adapter.SelectCommand = command;
-            // Filling the dataset form Excel using OleDbAdapter
+
+            var wbook = new XLWorkbook(path_file_xls);
+            var workSheet = wbook.Worksheet(3);
+
+            //Create a new DataTable.
+            DataTable dt = new DataTable();
+
+            //Loop through the Worksheet rows.
+            bool firstRow = true;
+            foreach (IXLRow row in workSheet.Rows())
+            {
+                //Use the first row to add columns to DataTable.
+                if (firstRow)
+                {
+                    foreach (IXLCell cell in row.Cells())
+                    {
+                        dt.Columns.Add(cell.Value.ToString());
+                    }
+                    firstRow = false;
+                }
+                else
+                {
+                    //Add rows to DataTable.
+                    dt.Rows.Add();
+                    int i = 0;
+                    foreach (IXLCell cell in row.Cells())
+                    {
+                        dt.Rows[dt.Rows.Count - 1][i] = cell.Value.ToString();
+                        i++;
+                    }
+                }
+
+            }
+
             DataSet data = new DataSet();
-            adapter.Fill(data);
-            System.Diagnostics.Debug.WriteLine(data.Tables[0].Rows[0][0]);
-            // Closing the connection
-            connection.Close();
+            data.Tables.Add(dt);
+
             return data;
         }
-
 
 
     }
